12e5f366b37f30c2c1fdd40acc29ade6383fc762
TJ Tanjai <tanjai_k@hotmail.com>
2015-10-18 22:06:43 +0700

    Add Exercise

---
+++ Ex3_Smarter_Weather.py
@@ -0,0 +1,14 @@
+#please enter weather
+weather = raw_input("How the weather today? ")
+
+#if the weather is greater than or equal to 25 degrees
+if weather >= 25 :
+    print ("Go to the beach! ")
+
+#if the weather is less than 25 degrees And Greater than 15 degrees
+elif weather < 215 :
+    print ("Stay home")
+
+else :
+    print ("Do nothing :D ")
+
